5c1e88808c32a0f5756de5a1d7dda849d10d344c
salinas182 <salinas.182@hotmail.com>
2022-02-10 17:53:38 -0400

    listed entries info from the api

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,23 +1,4 @@
 export default () => ({
     isLoading: true,
-    entries: [
-        {
-            id: '1',
-            date: new Date().toDateString(),
-            text: 'gdfbhjabhjasbfasbdskjbgkjsgbndklsgbndklsgbndklgbdklsbgdklsbgldsgbndkls sdkgndklgndklsgndsklgndsklgndskl gdsklngkldsngkdlsngdklsgndklsng dskgndklsgndsklgnsdl',
-            picture: null
-        },
-        {
-            id: '2',
-            date: new Date().toDateString(),
-            text: 'gdfbhjabhjasbfasbdskjbgkjsgbndklsgbndklsgbndklgbdklsbgdklsbgldsgbndkls',
-            picture: null
-        },
-        {
-            id: '3',
-            date: new Date().toDateString(),
-            text: 'gdfbhjabhjasbfasbdskjbgkjsgbndklsgbndklsgbndklgbdklsbgdklsbgldsgbndkls sdkgndklgndklsgndsklgndsklgndskl gdsklngkldsngkdlsngdklsgndklsng dskgndklsgndsklgnsdlvfdhfdhdfhdsfhdfjhgfjgfjgf',
-            picture: null
-        }
-    ]
+    entries: []
 })
