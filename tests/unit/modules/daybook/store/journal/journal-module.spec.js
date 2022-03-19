312e080dab1e0a92d49903ad6d781486796a4b9f
Rojiblanco182 <salinas.182@hotmail.com>
2022-03-20 00:54:08 +0100

    tested mutations

--- tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -20,4 +20,54 @@ describe('Journal Module Store', () => {
     expect(isLoading).toBeFalsy()
     expect(entries).toEqual(journalState.entries)
   })
+
+  test('should fill the store entries with the ones we send to the store', () => {
+    const mockStore = createVuexStore({ isLoading: true, entries: [] })
+
+    expect(mockStore.state.journal.isLoading).toBeTruthy()
+    expect(mockStore.state.journal.entries.length).toBe(0)
+
+    mockStore.commit('journal/setEntries', journalState.entries)
+
+    expect(mockStore.state.journal.isLoading).toBeFalsy()
+    expect(mockStore.state.journal.entries.length).toBe(3)
+  })
+
+  test('should update the store entries if we modify them', () => {
+    const updatedEntry = { ...journalState.entries[0], text: 'mock text' }
+    const mockStore = createVuexStore(journalState)
+    let entries = mockStore.state.journal.entries
+    
+    expect(entries.length).toBe(3)
+    
+    mockStore.commit('journal/updateEntry', updatedEntry)
+    entries = mockStore.state.journal.entries
+    const targetEntry = entries.find((entry) => entry.id === updatedEntry.id)
+
+    expect(entries.length).toBe(3)
+    expect(targetEntry).toEqual(updatedEntry)
+  })
+
+  test('should be able to add and delete an specific entry', () => {
+    const newEntry = {
+      id: 'mockId',
+      text: 'mock text'
+    }
+    const mockStore = createVuexStore(journalState)
+    let entries = mockStore.state.journal.entries
+
+    expect(entries.length).toBe(3)
+
+    mockStore.commit('journal/addEntry', newEntry)
+    entries = mockStore.state.journal.entries
+
+    expect(entries.length).toBe(4)
+    expect(entries.find((entry) => entry.id === newEntry.id)).toEqual(newEntry)
+
+    mockStore.commit('journal/deleteEntry', newEntry.id)
+    entries = mockStore.state.journal.entries
+
+    expect(entries.length).toBe(3)
+    expect(entries.find((entry) => entry.id === newEntry.id)).toBeUndefined()
+  })
 })
