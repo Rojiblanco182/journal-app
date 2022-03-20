8e3b5ac3ffef773f69d7322cf6ac74902e8c45c7
Rojiblanco182 <salinas.182@hotmail.com>
2022-03-20 12:28:48 +0100

    tested the store getters

--- tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -21,53 +21,78 @@ describe('Journal Module Store', () => {
     expect(entries).toEqual(journalState.entries)
   })
 
-  test('should fill the store entries with the ones we send to the store', () => {
-    const mockStore = createVuexStore({ isLoading: true, entries: [] })
-
-    expect(mockStore.state.journal.isLoading).toBeTruthy()
-    expect(mockStore.state.journal.entries.length).toBe(0)
-
-    mockStore.commit('journal/setEntries', journalState.entries)
-
-    expect(mockStore.state.journal.isLoading).toBeFalsy()
-    expect(mockStore.state.journal.entries.length).toBe(3)
+  describe('Mutations', () => {
+    test('should fill the store entries with the ones we send to the store', () => {
+      const mockStore = createVuexStore({ isLoading: true, entries: [] })
+  
+      expect(mockStore.state.journal.isLoading).toBeTruthy()
+      expect(mockStore.state.journal.entries.length).toBe(0)
+  
+      mockStore.commit('journal/setEntries', journalState.entries)
+  
+      expect(mockStore.state.journal.isLoading).toBeFalsy()
+      expect(mockStore.state.journal.entries.length).toBe(3)
+    })
+  
+    test('should update the store entries if we modify them', () => {
+      const updatedEntry = { ...journalState.entries[0], text: 'mock text' }
+      const mockStore = createVuexStore(journalState)
+      let entries = mockStore.state.journal.entries
+      
+      expect(entries.length).toBe(3)
+      
+      mockStore.commit('journal/updateEntry', updatedEntry)
+      entries = mockStore.state.journal.entries
+      const targetEntry = entries.find((entry) => entry.id === updatedEntry.id)
+  
+      expect(entries.length).toBe(3)
+      expect(targetEntry).toEqual(updatedEntry)
+    })
+  
+    test('should be able to add and delete an specific entry', () => {
+      const newEntry = {
+        id: 'mockId',
+        text: 'mock text'
+      }
+      const mockStore = createVuexStore(journalState)
+      let entries = mockStore.state.journal.entries
+  
+      expect(entries.length).toBe(3)
+  
+      mockStore.commit('journal/addEntry', newEntry)
+      entries = mockStore.state.journal.entries
+  
+      expect(entries.length).toBe(4)
+      expect(entries.find((entry) => entry.id === newEntry.id)).toEqual(newEntry)
+  
+      mockStore.commit('journal/deleteEntry', newEntry.id)
+      entries = mockStore.state.journal.entries
+  
+      expect(entries.length).toBe(3)
+      expect(entries.find((entry) => entry.id === newEntry.id)).toBeUndefined()
+    })
   })
 
-  test('should update the store entries if we modify them', () => {
-    const updatedEntry = { ...journalState.entries[0], text: 'mock text' }
-    const mockStore = createVuexStore(journalState)
-    let entries = mockStore.state.journal.entries
-    
-    expect(entries.length).toBe(3)
-    
-    mockStore.commit('journal/updateEntry', updatedEntry)
-    entries = mockStore.state.journal.entries
-    const targetEntry = entries.find((entry) => entry.id === updatedEntry.id)
+  describe('Getters', () => {
+    test('should be able to fetch an entry containing the user input or all of them if no input is provided', () => {
+      const mockStore = createVuexStore(journalState)
+      const mockInput = 'mucho'
+      const targetEntry = mockStore.state.journal.entries.find((entry) => entry.text.includes(mockInput))
 
-    expect(entries.length).toBe(3)
-    expect(targetEntry).toEqual(updatedEntry)
-  })
+      expect(mockStore.getters['journal/getEntriesByTerm']('').length).toBe(3)
+      expect(mockStore.getters['journal/getEntriesByTerm']('')).toEqual(journalState.entries)
 
-  test('should be able to add and delete an specific entry', () => {
-    const newEntry = {
-      id: 'mockId',
-      text: 'mock text'
-    }
-    const mockStore = createVuexStore(journalState)
-    let entries = mockStore.state.journal.entries
+      expect(mockStore.getters['journal/getEntriesByTerm'](mockInput).length).toBe(1)
+      expect(mockStore.getters['journal/getEntriesByTerm'](mockInput)).toEqual([targetEntry])
+    })
 
-    expect(entries.length).toBe(3)
+    test('should be able to fetch an entry by ID or undefined if no ID is provided', () => {
+      const targetEntry = journalState.entries[0]
+      const mockStore = createVuexStore(journalState)
 
-    mockStore.commit('journal/addEntry', newEntry)
-    entries = mockStore.state.journal.entries
+      expect(mockStore.getters['journal/getEntryById']('')).toBeUndefined()
 
-    expect(entries.length).toBe(4)
-    expect(entries.find((entry) => entry.id === newEntry.id)).toEqual(newEntry)
-
-    mockStore.commit('journal/deleteEntry', newEntry.id)
-    entries = mockStore.state.journal.entries
-
-    expect(entries.length).toBe(3)
-    expect(entries.find((entry) => entry.id === newEntry.id)).toBeUndefined()
+      expect(mockStore.getters['journal/getEntryById'](targetEntry.id)).toEqual(targetEntry)
+    })
   })
 })
