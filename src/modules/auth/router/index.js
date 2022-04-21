62b993ac00cf4751f784bbb21902d7ec158e8eb2
Rojiblanco182 <salinas.182@hotmail.com>
2022-04-21 15:05:05 +0200

    added auth routes and initial state

--- src/modules/auth/router/index.js
+++ src/modules/auth/router/index.js
@@ -1,5 +1,16 @@
 export default {
   name: 'auth',
   component: () => import(/* webpackChunkName: "auth layout" */ '@/modules/auth/layouts/AuthLayout.vue'),
-  children: [],
+  children: [
+    {
+      path: '',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "login" */ '@/modules/auth/views/Login.vue'),
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import(/* webpackChunkName: "signup" */ '@/modules/auth/views/Signup.vue'),
+    },
+  ],
 }
