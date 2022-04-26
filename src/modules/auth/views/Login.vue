9f42929188b8facadadd0baff2985a475d3ec5fb
Rojiblanco182 <salinas.182@hotmail.com>
2022-04-27 00:02:08 +0200

    added user login

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -2,15 +2,15 @@
   <span class="login100-form-title p-b-41">
 		Ingresar
 	</span>
-	<form class="login100-form validate-form p-b-33 p-t-5">
+	<form class="login100-form validate-form p-b-33 p-t-5" @submit.prevent="onSubmit">
 
 		<div class="wrap-input100 validate-input" data-validate = "Enter username">
-			<input class="input100" type="text" placeholder="Correo" required>
+			<input v-model="userForm.email" class="input100" type="text" placeholder="Correo" required>
 			<span class="focus-input100" data-placeholder="&#xe82a;"></span>
 		</div>
 
 		<div class="wrap-input100 validate-input" data-validate="Enter password">
-			<input class="input100" type="password" placeholder="Contraseña" required>
+			<input v-model="userForm.password" class="input100" type="password" placeholder="Contraseña" required>
 			<span class="focus-input100" data-placeholder="&#xe80f;"></span>
 		</div>
 
@@ -27,7 +27,28 @@
 </template>
 
 <script>
-export default {
+import { useRouter } from 'vue-router'
+import { ref } from 'vue';
+import Swal from 'sweetalert2';
+import useAuth from '@/modules/auth/composables/useAuth';
 
+export default {
+	setup() {
+		const router = useRouter()
+		const { loginUser } = useAuth()
+		const userForm = ref({
+			email: '',
+			password: ''
+		})
+
+		return {
+			userForm,
+			onSubmit: async () => {
+				const { ok, message } = await loginUser(userForm.value)
+				if (!ok) return Swal.fire('Error', message, 'error')
+				router.push({ name: 'no-entry' })
+			}
+		}
+	}
 }
 </script>
