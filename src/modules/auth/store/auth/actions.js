06c5c85c16790f3ac5d7521ec9130ba0d722448d
Rojiblanco182 <salinas.182@hotmail.com>
2022-04-26 22:43:00 +0200

    added user sign-up

--- src/modules/auth/store/auth/actions.js
+++ src/modules/auth/store/auth/actions.js
@@ -1,1 +1,19 @@
-//export const myAction = async ({ commit }) => {}
+import authApi from "@/api/authApi"
+
+export const createUser = async ({ commit }, user) => {
+  const { name, email, password } = user
+
+  try {
+    const { data } = await authApi.post(':signUp', { email, password, returnSecureToken: true })
+    const { idToken, refreshToken } = data
+
+    await authApi.post(':update', { displayName: name, idToken })
+
+    delete user.password
+    commit('loginUser', { user, idToken, refreshToken })
+
+    return { ok: true }
+  } catch (error) {
+    return { ok: false, message: error.response.data.error.message }
+  }
+}
--- src/modules/auth/views/Signup.vue
+++ src/modules/auth/views/Signup.vue
@@ -2,25 +2,25 @@
   <span class="login100-form-title p-b-41">
 		Registro
 	</span>
-	<form class="login100-form validate-form p-b-33 p-t-5">
+	<form class="login100-form validate-form p-b-33 p-t-5" @submit.prevent="onSubmit" >
 
 		<div class="wrap-input100 validate-input" data-validate = "Enter username">
-			<input class="input100" type="text" placeholder="Nombre" required>
+			<input v-model="userForm.username" class="input100" type="text" placeholder="Nombre" required>
 			<span class="focus-input100" data-placeholder="&#xe82a;"></span>
 		</div>
 
 		<div class="wrap-input100 validate-input" data-validate = "Enter email">
-			<input class="input100" type="text" placeholder="Correo" required>
+			<input v-model="userForm.email" class="input100" type="email" placeholder="Correo" required>
 			<span class="focus-input100" data-placeholder="&#xe818;"></span>
 		</div>
 
 		<div class="wrap-input100 validate-input" data-validate="Enter password">
-			<input class="input100" type="password" placeholder="Contraseña" required>
+			<input v-model="userForm.password" class="input100" type="password" placeholder="Contraseña" required>
 			<span class="focus-input100" data-placeholder="&#xe80f;"></span>
 		</div>
 
 		<div class="container-login100-form-btn m-t-32">
-			<button class="login100-form-btn">
+			<button type="submit" class="login100-form-btn">
 				Sign up
 			</button>
 		</div>
@@ -32,7 +32,29 @@
 </template>
 
 <script>
-export default {
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import useAuth from '@/modules/auth/composables/useAuth'
+import Swal from 'sweetalert2'
 
+export default {
+	setup() {
+		const router = useRouter()
+		const { createUser } = useAuth()
+		const userForm = ref({
+			username: '',
+			email: '',
+			password: '',
+		});
+
+		return { 
+			userForm,
+			onSubmit: async () => {
+				const { ok, message } = await createUser(userForm.value)
+				if (!ok) return Swal.fire('Error', message, 'error')
+				router.push({ name: 'no-entry' })
+			}
+		};
+	}
 }
 </script>
