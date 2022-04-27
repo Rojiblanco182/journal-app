4012a87a5a21c2c6bbf7f10d9f85ba6245e0b2d2
Rojiblanco182 <salinas.182@hotmail.com>
2022-04-27 17:37:10 +0200

    added axios interceptor

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,4 +4,11 @@ const journalApi = axios.create({
     baseURL: 'https://journal-app-83031-default-rtdb.europe-west1.firebasedatabase.app/'
 })
 
+journalApi.interceptors.request.use((config) => {
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+    return config
+})
+
 export default journalApi
