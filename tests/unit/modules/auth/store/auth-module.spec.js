775633ad689d9afb16e3dee37e52717106335e01
Rojiblanco182 <salinas.182@hotmail.com>
2022-05-06 23:32:28 +0200

    tested actions

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -1,4 +1,5 @@
 import createVuexStore from "../../../mock-data/mock-store"
+import authApi from '@/api/authApi'
 
 describe('Auth-module', () => {
   test('should have a initial state', () => {
@@ -17,53 +18,189 @@ describe('Auth-module', () => {
     expect(refreshToken).toBe(null)
   })
 
-  //MUTATIONS
-  test('Mutation: loginUser', () => {
-    const store = createVuexStore({
-      status: 'authenticating',
-      user: null,
-      idToken: null,
-      refreshToken: null,
+  describe('Mutations', () => {
+    test('loginUser', () => {
+      const store = createVuexStore({
+        status: 'authenticating',
+        user: null,
+        idToken: null,
+        refreshToken: null,
+      })
+      const payload = {
+        user: { username: 'mockUser', email: 'mockEmail@domain.com' },
+        idToken: 'mockToken-123',
+        refreshToken: 'mockRefresh-123'
+      }
+  
+      store.commit('authModule/loginUser', payload)
+  
+      const { status, user, idToken, refreshToken } = store.state.authModule
+  
+      expect(status).toBe('authenticated')
+      expect(user).toStrictEqual(payload.user)
+      expect(idToken).toBe(payload.idToken)
+      expect(refreshToken).toBe(payload.refreshToken)
     })
-    const payload = {
-      user: { username: 'mockUser', email: 'mockEmail@domain.com' },
-      idToken: 'mockToken-123',
-      refreshToken: 'mockRefresh-123'
-    }
-
-    store.commit('authModule/loginUser', payload)
-
-    const { status, user, idToken, refreshToken } = store.state.authModule
-
-    expect(status).toBe('authenticated')
-    expect(user).toStrictEqual(payload.user)
-    expect(idToken).toBe(payload.idToken)
-    expect(refreshToken).toBe(payload.refreshToken)
+  
+    test('logout', () => {
+      const initialState = {
+        status: 'authenticated',
+        user: { username: 'mockUser', email: 'mockEmail@domain.com' },
+        idToken: 'mockToken-123',
+        refreshToken: 'mockRefresh-123'
+      }
+      const store = createVuexStore(initialState)
+  
+      localStorage.setItem('idToken', initialState.idToken)
+      localStorage.setItem('refreshToken', initialState.refreshToken)
+  
+      expect(localStorage.getItem('idToken')).toBe(initialState.idToken)
+  
+      store.commit('authModule/logout')
+  
+      const { status, user, idToken, refreshToken } = store.state.authModule
+  
+      expect(status).toBe('not-authenticated')
+      expect(user).toBe(null)
+      expect(idToken).toBe(null)
+      expect(refreshToken).toBe(null)
+      expect(localStorage.getItem('idToken')).toBe(null)
+      expect(localStorage.getItem('refreshToken')).toBe(null)
+    })
   })
 
-  test('Mutation: logout', () => {
-    const initialState = {
-      status: 'authenticated',
-      user: { username: 'mockUser', email: 'mockEmail@domain.com' },
-      idToken: 'mockToken-123',
-      refreshToken: 'mockRefresh-123'
-    }
-    const store = createVuexStore(initialState)
+  describe('Getters', () => {
+    test('authStatus', () => {
+      const initialState = {
+        status: 'authenticated',
+        user: { username: 'mockUser', email: 'mockEmail@domain.com' },
+        idToken: 'mockToken-123',
+        refreshToken: 'mockRefresh-123'
+      }
+      const store = createVuexStore(initialState)
+      const result = store.getters['authModule/authStatus']
 
-    localStorage.setItem('idToken', initialState.idToken)
-    localStorage.setItem('refreshToken', initialState.refreshToken)
+      expect(result).toBe(initialState.status)
+    })
 
-    expect(localStorage.getItem('idToken')).toBe(initialState.idToken)
+    test('username', () => {
+      const initialState = {
+        status: 'authenticated',
+        user: { username: 'mockUser', email: 'mockEmail@domain.com' },
+        idToken: 'mockToken-123',
+        refreshToken: 'mockRefresh-123'
+      }
+      const store = createVuexStore(initialState)
+      const result = store.getters['authModule/username']
 
-    store.commit('authModule/logout')
+      expect(result).toBe(initialState.user.username)
+    })
+  })
 
-    const { status, user, idToken, refreshToken } = store.state.authModule
+  describe('Actions', () => {
+    describe('Create user', () => {
+      test('User already exists', async () => {
+        const initialState = {
+          status: 'not-authenticated',
+          user: null,
+          idToken: null,
+          refreshToken: null
+        }
+        const newUser = { username: 'mockUser', email: 'test@test.com', password: '123456' }
+        const store = createVuexStore(initialState)
+        const response = await store.dispatch('authModule/createUser', newUser)
+        const expectedResult = { ok: false, message: 'EMAIL_EXISTS' }
+        const { status, user, idToken, refreshToken } = store.state.authModule
 
-    expect(status).toBe('not-authenticated')
-    expect(user).toBe(null)
-    expect(idToken).toBe(null)
-    expect(refreshToken).toBe(null)
-    expect(localStorage.getItem('idToken')).toBe(null)
-    expect(localStorage.getItem('refreshToken')).toBe(null)
+        expect(status).toBe('not-authenticated')
+        expect(user).toBe(null)
+        expect(idToken).toBe(null)
+        expect(refreshToken).toBe(null)
+        expect(localStorage.getItem('idToken')).toBe(null)
+        expect(localStorage.getItem('refreshToken')).toBe(null)
+        expect(response).toEqual(expectedResult)
+      })
+
+      test('User created', async () => {
+        const initialState = {
+          status: 'not-authenticated',
+          user: null,
+          idToken: null,
+          refreshToken: null
+        }
+        const newUser = { username: 'mockUser', email: 'test2@test.com', password: '123456' }
+        const store = createVuexStore(initialState)
+        //Sign in already existing user in order to get their idToken
+        await store.dispatch('authModule/signInUser', newUser)
+
+        const { idToken } = store.state.authModule
+        //Delete the test user so that we can create it again
+        await authApi.post(':delete', { idToken })
+
+        //Create a new user
+        const result = await store.dispatch('authModule/createUser', newUser)
+        const expectedResult = { ok: true }
+
+        const { status, user, idToken: token, refreshToken } = store.state.authModule
+
+        expect(status).toBe('authenticated')
+        expect(user).toMatchObject({ username: newUser.username, email: newUser.email })
+        expect(typeof token).toBe('string')
+        expect(typeof refreshToken).toBe('string')
+        expect(result).toEqual(expectedResult)
+      })
+    })
+
+    describe('checkAuthentication', () => {
+      test('Authenticated', async () => {
+        const store = createVuexStore({
+          status: 'not-authenticated',
+          user: null,
+          idToken: null,
+          refreshToken: null,
+        })
+        const mockUser = { username: 'Test User', email: 'test@test.com', password: '123456' }
+
+        await store.dispatch('authModule/signInUser', mockUser)
+        const { idToken } = store.state.authModule
+
+        store.commit('authModule/logout')
+        localStorage.setItem('idToken', idToken)
+        
+        const checkResp = await store.dispatch('authModule/checkAuthentication')
+        const { status, user, idToken: token } = store.state.authModule
+
+        expect(checkResp).toEqual({ ok: true })
+
+        expect(status).toBe('authenticated')
+        expect(user).toMatchObject({ username: mockUser.username, email: mockUser.email })
+        expect(typeof token).toBe('string')
+      })
+
+      test('Not authenticated', async () => {
+        const store = createVuexStore({
+          status: 'authenticating',
+          user: null,
+          idToken: null,
+          refreshToken: null,
+        })
+
+        const noTokenExpectedResult = { ok: false, message: 'User token is missing' }
+
+        localStorage.removeItem('idToken')
+        const noTokenAuthResp = await store.dispatch('authModule/checkAuthentication')
+
+        expect(noTokenAuthResp).toEqual(noTokenExpectedResult)
+        expect(store.state.authModule.status).toBe('not-authenticated')
+
+        const wrongTokenExpectedResult = { ok: false, message: 'INVALID_ID_TOKEN' }
+
+        localStorage.setItem('idToken', 'mockToken')
+        const wrongTokenAuthResp = await store.dispatch('authModule/checkAuthentication')
+
+        expect(wrongTokenAuthResp).toEqual(wrongTokenExpectedResult)
+        expect(store.state.authModule.status).toBe('not-authenticated')
+      })
+    })
   })
 })
